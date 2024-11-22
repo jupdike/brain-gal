d82a7c6742873c87038422cf2dff6b022a0b5c57
Jared Updike <jupdike@thebrain.com>
2024-11-21 21:01:30 -0800

    Initial work in progress for inspecting elements inside Thought
    to find URLs and Parents, Parents of Parents, Tags, etc.

--- example/a.jsx
+++ example/a.jsx
@@ -1,8 +1,4 @@
+// just pass URLs up the chain as props
 const A = (props) => {
-    let lower = props.href.toLowerCase();
-    if(lower.endsWith('.png') || lower.endsWith('.jpg') || lower.endsWith('.gif')) {
-        return <img src={props.href} class="poster" />;
-    }
     return <a href={props.href}>URL here</a>;
 }
-
--- example/thought.jsx
+++ example/thought.jsx
@@ -1,6 +1,24 @@
 // keep a parent, but only if parent of parent (pop) is one of these
-const popKeeprs = ["By Tag", "Person", "Organization"];
-const Thought = (props) => <div class="card rounded" data-title={props.title}>
-    <div>{props.children}</div>
-    <div class="text-l saira-card-title">{props.title.substring(5)}</div>
-</div>
+const popKeepers = ["By Tag", "Person", "Organization"];
+
+function isImg(x) {
+    let lower = x.toLowerCase();
+    return lower.endsWith('.png') || lower.endsWith('.jpg') || lower.endsWith('.gif');
+}
+
+function getJson(props) {
+    let ret = {};
+    let guts = BrayElem.childrenWithoutWhitespaceStrings(props.children).map(x => x.props);
+    guts.filter((x) => x.href && isImg(x.href)).forEach(x =>  { ret.imgsrc = x.href; });
+    guts.filter((x) => x.href && !isImg(x.href)).forEach(x => { ret.href = x.href; });
+    //console.error('getJson:', ret);
+    return ret;
+}
+
+const Thought = (props) => {
+    const json = getJson(props);
+    return <div class="card rounded" data-title={props.title}>
+        <div>{props.children}</div>
+        <a href={json.href}><div class="text-l saira-card-title">{props.title.substring(5)}</div></a>
+    </div>
+}
